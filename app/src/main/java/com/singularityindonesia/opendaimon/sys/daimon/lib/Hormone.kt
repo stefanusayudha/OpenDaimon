723ee31d460cad89b7e78844daaf26499d80f0e7
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-01-19 09:30:32 +0700

    chore: restructure

--- app/src/main/java/com/singularityindonesia/opendaimon/sys/daimon/lib/Hormone.kt
+++ app/src/main/java/com/singularityindonesia/opendaimon/sys/daimon/lib/Hormone.kt
@@ -1,49 +1,26 @@
 package com.singularityindonesia.opendaimon.sys.daimon.lib
 
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 
 @JvmInline
 value class Cortisol(val value: Float)
 
-class CortisolGland(
-    private val scope: CoroutineScope
-) {
-    val hormoneLevel = MutableStateFlow(Cortisol(0f))
-}
-
 @JvmInline
 value class Adrenalin(val value: Float)
-
-class AdrenalGland(
-    private val scope: CoroutineScope
-) {
-    val hormoneLevel = MutableStateFlow(Adrenalin(0f))
-}
 
 @JvmInline
 value class Dopamine(val value: Float)
 
-class DopamineGland(
-    private val scope: CoroutineScope
-) {
-    val hormoneLevel = MutableStateFlow(Dopamine(0f))
-}
-
 @JvmInline
 value class Endorphin(val value: Float)
-
-class EndorphinGland(
-    private val scope: CoroutineScope
-) {
-    val hormoneLevel = MutableStateFlow(Endorphin(0f))
-}
 
 @JvmInline
 value class Serotonin(val value: Float)
 
-class SerotoninGland(
-    private val scope: CoroutineScope
-) {
-    val hormoneLevel = MutableStateFlow(Serotonin(0f))
+class HormoneGland {
+    val cortisol = MutableStateFlow(Cortisol(0f))
+    val adrenalin = MutableStateFlow(Adrenalin(0f))
+    val dopamine = MutableStateFlow(Dopamine(0f))
+    val endorphin = MutableStateFlow(Endorphin(0f))
+    val serotonin = MutableStateFlow(Serotonin(0f))
 }
