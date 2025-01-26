793a5a0e4cf979fcc3fce951716a712c87a5504c
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-01-26 09:58:40 +0700

    feat: cns: add neuron concept

--- lib/cns/src/main/java/com/singularityindonesia/cns/neuron/Neuron.kt
+++ lib/cns/src/main/java/com/singularityindonesia/cns/neuron/Neuron.kt
@@ -1,10 +1,20 @@
 package com.singularityindonesia.cns.neuron
 
 import com.singularityindonesia.cns.lib.Position3D
-typealias NeuronID = Double
+
+typealias NeuronID = Long
+typealias Weight = Double
 
 data class Neuron(
     val position: Position3D,
     val dendritConnections: Array<Pair<Signal, NeuronID>>,
-    val synapseConnection: Array<Pair<Signal,NeuronID>>
-)
+    val synapseConnection: Array<Pair<Signal, NeuronID>>
+) {
+    private suspend fun forward(signal: Signal): Result<Pair<NeuronID, Weight>> {
+        return TODO("Forward signal to neuron")
+    }
+
+    suspend fun ping(signal: Signal): Result<Weight> {
+        return TODO("Send signal to neuron and return calculated weight")
+    }
+}
